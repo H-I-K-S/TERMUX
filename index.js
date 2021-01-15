50a36fec9529e4afbee96dc4e67b373a83b542e2
ナスワサルサ <75057738+NazwaS@users.noreply.github.com>
2021-01-15 19:42:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1622,6 +1622,16 @@ async function starts() {
                                         }
                                         client.sendMessage(from, tag, text, {quoted: mek})
                                         break
+                                case 'shadow':
+                                        var gh = body.slice(7)
+                                        var teks1 = gh.split("|")[0];
+                                        if (args.length < 1) return reply('teksnya mana um\nContoh: ${prefix}shadow NazwaCanss
+                                        if (!isUser) return reply(mess.only.daftarB)
+                                        reply(mess.wait)
+                                        anu = await fetchJson(`https://tobz-api.herokuapp.com/api/photooxy?theme=shadow&text=${text1}&apikey=BotWeA`, {method: 'get'})
+                                        buffer = await getBuffer(anu.result)
+                                        client.sendMessage(from, buffer, image, {quoted: mek, caption: 'Nih kak gambarnya...'})
+                                        break
 				case 'wait':
 					if ((isMedia && !mek.message.videoMessage || isQuotedImage) && args.length == 0) {
                                         if (!isUser) return reply(mess.only.daftarB)
