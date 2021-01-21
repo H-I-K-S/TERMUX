e7195a5310369529f4d1e9889df9f7ef8c01dcb6
ナスワサルサ <75057738+NazwaS@users.noreply.github.com>
2021-01-21 17:48:50 +0700

    Create 18+.js

---
+++ database/menu/18+.js
@@ -0,0 +1,20 @@
+const 18+ = (prefix, ownerBot, botName) => {
+        return `
+「 *${botName}* 」
+◪ *INFO*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Creator: ${ownerBot}
+◪ *ABOUT*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+◪ *18+*
+  |
+  ├─ ❏ ${prefix}randomhentai
+  ├─ ❏ ${prefix}nsfwtrap
+  └─ ❏ ${prefix}nsfwneko`
+}
+exports.18+ = 18+
