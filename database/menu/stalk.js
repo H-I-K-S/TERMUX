329a5713f2564cdbc0b4118d18a498a0f24c3587
ナスワサルサ <75057738+NazwaS@users.noreply.github.com>
2021-01-21 17:42:20 +0700

    Create stalk.js

---
+++ database/menu/stalk.js
@@ -0,0 +1,19 @@
+const stalk = (prefix, ownerBot, botName) => {
+        return `
+「 *${botName}* 」
+◪ *INFO*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Creator: ${ownerBot}
+◪ *ABOUT*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+◪ *STALK*
+  │
+  ├─ ❏ ${prefix}tiktokstalk
+  └─ ❏ ${prefix}igstalk`
+}
+exports.stalk = stalk
