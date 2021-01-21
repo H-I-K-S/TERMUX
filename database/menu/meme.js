dd81d407f1096008b0b5b636b29f603275e9bcab
ナスワサルサ <75057738+NazwaS@users.noreply.github.com>
2021-01-21 17:27:35 +0700

    Create meme.js

---
+++ database/menu/meme.js
@@ -0,0 +1,19 @@
+const meme = (prefix, botName, ownerName) => {
+	return `
+「 *${botName}* 」
+◪ *INFO*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Creator: ${ownerName}
+◪ *ABOUT*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+◪ *MEME*
+  │
+  ├─ ❏ ${prefix}meme
+  └─ ❏ ${prefix}memeindo`
+}
+exports.meme = meme
