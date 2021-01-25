02ae0dfd283760726feab0f0367d61845161ba04
ナスワサルサ <75057738+NazwaS@users.noreply.github.com>
2021-01-25 09:04:58 +0700

    Update group.js

--- database/menu/group.js
+++ database/menu/group.js
@@ -1,10 +1,11 @@
-const group = (prefix, ownerBot, botName) => {
+const group = (prefix, botName, ownerName) => {
         return `
 「 *${botName}* 」
 
 ◪ *INFO*
   ❏ Prefix: 「  ${prefix}  」
-  ❏ Creator: ${ownerBot}
+  ❏ Creator: ${ownerName}
+  ❏ Version: 0.0.4
 ◪ *ABOUT*
   │
   ├─ ❏ ${prefix}info
