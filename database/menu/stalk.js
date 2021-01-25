87c73c60aee29ac17db3bf563af3e379f5d15c1f
ナスワサルサ <75057738+NazwaS@users.noreply.github.com>
2021-01-25 09:24:19 +0700

    Update stalk.js

--- database/menu/stalk.js
+++ database/menu/stalk.js
@@ -1,10 +1,11 @@
-const stalk = (prefix, ownerBot, botName) => {
+const stalk = (prefix, botName, ownerName) => {
         return `
 「 *${botName}* 」
 
 ◪ *INFO*
   ❏ Prefix: 「  ${prefix}  」
-  ❏ Creator: ${ownerBot}
+  ❏ Creator: ${ownerName}
+  ❏ Version: 0.0.4
 ◪ *ABOUT*
   │
   ├─ ❏ ${prefix}info
