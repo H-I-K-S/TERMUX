69c12bfe38b3dde44cbcad286aeb4e22f42dd5bf
ナスワサルサ <75057738+NazwaS@users.noreply.github.com>
2021-01-25 09:31:02 +0700

    Update wibu.js

--- database/menu/wibu.js
+++ database/menu/wibu.js
@@ -1,10 +1,11 @@
-const wibu = (prefix, ownerBot, botName) => {
+const wibu = (prefix, botName, ownerName) => {
         return `
 「 *${botName}* 」
 
 ◪ *INFO*
   ❏ Prefix: 「  ${prefix}  」
-  ❏ Creator: ${ownerBot}
+  ❏ Creator: ${ownerName}
+  ❏ Version: 0.0.4
 ◪ *ABOUT*
   │
   ├─ ❏ ${prefix}info
