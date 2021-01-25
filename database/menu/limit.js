6378774123c46469db05d2651b53e21c089eee79
ナスワサルサ <75057738+NazwaS@users.noreply.github.com>
2021-01-25 09:14:39 +0700

    Update limit.js

--- database/menu/limit.js
+++ database/menu/limit.js
@@ -1,10 +1,11 @@
-const limit = (prefix, ownerBot, botName) => {
+const limit = (prefix, botName, ownerName) => {
         return `
 「 *${botName}* 」
 
 ◪ *INFO*
   ❏ Prefix: 「  ${prefix}  」
-  ❏ Creator: ${ownerBot}
+  ❏ Creator: ${ownerName}
+  ❏ Version: 0.0.4
 ◪ *ABOUT*
   │
   ├─ ❏ ${prefix}info
