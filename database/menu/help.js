0e93133a9874d2ad1bff3ae114eed327f2ccbcdc
Human <77788392+H-I-K-S@users.noreply.github.com>
2021-01-28 11:43:02 +0700

    Update help.js

--- database/menu/help.js
+++ database/menu/help.js
@@ -2,46 +2,55 @@ const help = (pushname, prefix, botName, ownerName, reqXp, uangku) => {
         return `
 「 *${botName}* 」
 
-◪ *USER INFO*
-  ❏ Name: ${pushname}
-  ❏ XP: ${reqXp}
-  ❏ Money: ${uangku}
-  ❏ Registered: ✔️
-◪ *BOT INFO*
-  ❏ Prefix: 「  ${prefix}  」
-  ❏ Creator: ${ownerName}
-  ❏ Version: 0.0.4
-◪ *ABOUT*
-  │
-  ├─ ❏ ${prefix}info
-  ├─ ❏ ${prefix}blocklist
-  ├─ ❏ ${prefix}chatlist
-  ├─ ❏ ${prefix}ping
-  └─ ❏ ${prefix}bugreport
-◪ *MENU*
-  │
-  ├─ ❏ ${prefix}logomakermenu
-  ├─ ❏ ${prefix}imagemakermenu
-  ├─ ❏ ${prefix}stickermakermenu
-  ├─ ❏ ${prefix}searchmenu
-  ├─ ❏ ${prefix}educationmenu
-  ├─ ❏ ${prefix}kerangmenu
-  ├─ ❏ ${prefix}downloadermenu
-  ├─ ❏ ${prefix}mememenu
-  ├─ ❏ ${prefix}groupmenu
-  ├─ ❏ ${prefix}soundmenu
-  ├─ ❏ ${prefix}musicmenu
-  ├─ ❏ ${prefix}islammenu
-  ├─ ❏ ${prefix}stalkmenu
-  ├─ ❏ ${prefix}wibumenu
-  ├─ ❏ ${prefix}18+menu
-  ├─ ❏ ${prefix}funmenu
-  ├─ ❏ ${prefix}todmenu
-  ├─ ❏ ${prefix}informationmenu
-  ├─ ❏ ${prefix}stayonscreenmenu
-  ├─ ❏ ${prefix}xpmenu
-  ├─ ❏ ${prefix}limitmenu
-  ├─ ❏ ${prefix}ownermenu
-  └─ ❏ ${prefix}othermenu`
+◪ 「°❀ 〘 USER INFO 〙 ❀°」
+╰───────────────────
+├─  ❏ Name: ${pushname}
+•├─  ❏ XP: ${reqXp}
+├─  ❏ Duit: ${uangku}
+•├─  ❏ Registered: √
+╰───────────────────
+
+◪ 「°❀ 〘 INFO BOT 〙 ❀°」
+╰───────────────────
+├─  ❏ Prefix: 「  ${prefix}  」
+•├─  ❏ Creator: ${ownerName}
+├─  ❏ Version: 4.0
+╰────────────────────❍
+
+◪「°❀ 〘 ABOUT 〙 ❀°」
+╰───────────────────  
+├─ ❏ ${prefix}info
+•├─ ❏ ${prefix}blocklist
+├─ ❏ ${prefix}chatlist
+•├─ ❏ ${prefix}ping
+├─ ❏ ${prefix}bugreport
+╰───────────────────
+
+◪「°❀ 〘 MENU 〙 ❀°」
+╰───────────────────
+├─ ❏ ${prefix}logomakermenu
+•├─ ❏ ${prefix}imagemakermenu
+├─ ❏ ${prefix}stickermakermenu
+•├─ ❏ ${prefix}searchmenu
+├─ ❏ ${prefix}educationmenu
+•├─ ❏ ${prefix}kerangmenu
+├─ ❏ ${prefix}downloadermenu
+•├─ ❏ ${prefix}mememenu
+├─ ❏ ${prefix}groupmenu
+•├─ ❏ ${prefix}soundmenu
+├─ ❏ ${prefix}musicmenu
+•├─ ❏ ${prefix}islammenu
+├─ ❏ ${prefix}stalkmenu
+•├─ ❏ ${prefix}wibumenu
+├─ ❏ ${prefix}18+menu
+•├─ ❏ ${prefix}funmenu
+├─ ❏ ${prefix}todmenu
+•├─ ❏ ${prefix}informationmenu
+├─ ❏ ${prefix}stayonscreenmenu
+•├─ ❏ ${prefix}xpmenu
+├─ ❏ ${prefix}limitmenu
+•├─ ❏ ${prefix}ownermenu
+├─ ❏ ${prefix}othermenu`
+╰───────────────────
 }
 exports.help = help
